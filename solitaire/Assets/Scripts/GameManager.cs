d94c1c261b41687654e06d34e999f2132c42d382
rebdal1998 <rdalton666@msn.com>
2022-05-02 16:06:39 -0400

    basic postprocessing
    levels 1 and 2 now have a bloom effect, levels 2 and 3 both have a vignette effect

--- solitaire/Assets/Scripts/GameManager.cs
+++ solitaire/Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     static public  GameObject deck, foundation, tableau;
-    public GameObject winner, btns, strt; 
+    public GameObject winner, btns, strt, loser; 
     static Card tempCard;
     public Text scoreTxt, timeTxt;
     static List<Card> tempCards = new List<Card>(); 
@@ -17,16 +17,20 @@ public class GameManager : MonoBehaviour
     int t = 0;
     int time; 
     int score;
-    int finalTime; 
+    int finalTime;
+    static int lvlsCompleted = 0; 
     void Start()
     {
         deck = GameObject.Find("Deck");
         foundation = GameObject.Find("foundation");
         tableau = GameObject.Find("runs");
         winner.SetActive(false);
+        loser.SetActive(false);
         score = 0;
         scoreTxt.text = "Score: 0";
         timeTxt.gameObject.SetActive(false);
+        scoreTxt.gameObject.SetActive(false);
+        btns.SetActive(false);
         t = 0; 
     }
     public void remove20Points()
@@ -65,12 +69,11 @@ public class GameManager : MonoBehaviour
     public void startLevel()
     {
       strt.SetActive(false);
-      switch(SceneManager.GetActiveScene().name)
+        switch (SceneManager.GetActiveScene().name)
       {
           case "level1":
-              deck.GetComponent<Deck>().deal(1031606578);
-              break;
           case "level2":
+              deck.GetComponent<Deck>().deal(1031606578);
               break;
           case "level3":
               break;
@@ -85,7 +88,10 @@ public class GameManager : MonoBehaviour
         scoreTxt.text = "Score: " + score.ToString();
         time = 0;
         timeTxt.gameObject.SetActive(true); 
-        timeTxt.text = "Time: " + time.ToString(); 
+        timeTxt.text = "Time: " + time.ToString();
+        scoreTxt.gameObject.SetActive(true);
+        btns.SetActive(true);
+  
     }
     void deal()
     {
@@ -109,23 +115,62 @@ public class GameManager : MonoBehaviour
             scoreTxt.text = "Score: " + score.ToString();
             //t = 0;
         }
-        bool b = true;
-        for(int i = 0; i < 4; i ++)
+        bool b = true; 
+        if (!winner.activeSelf && !loser.activeSelf)
         {
-            if (foundation.transform.GetChild(i).GetComponent<foundation_stack>().getSize() != 13)
-                b = false; 
+            for (int i = 0; i < 4; i++)
+            {
+                if (foundation.transform.GetChild(i).GetComponent<foundation_stack>().getSize() != 13)
+                    b = false;
+            }
         }
-        if(b && !winner.activeSelf)
+        if (b && !winner.activeSelf && !loser.activeSelf)
         {
-            winner.SetActive(true);
-            btns.SetActive(false);
-            timeTxt.gameObject.SetActive(false);
-            scoreTxt.gameObject.SetActive(false);
-            finalTime = time;
-            score += (int)700000 / finalTime;
-            winner.transform.GetChild(0).gameObject.GetComponent<Text>().text = "\t\t You've won \nFinal Score: " + score.ToString() + "\n Final Time: " + finalTime.ToString();
+            switch (SceneManager.GetActiveScene().name)
+            {
+                case "game_free_play":
+                case "level1":
+                    winSequence();
+                    break;
+                case "level2":
+                    if (time <= 300)
+                        winSequence();
+                    else
+                        loseSequence();
+                    break;
+                case "level3":
 
+                    break;
+                case "level4":
+
+                    break;
+                case "level5":
+
+                    break;
+                default:
+                    break;
+            }
         }
+    }
+    void loseSequence()
+    {
+        loser.SetActive(true);
+        btns.SetActive(false);
+        timeTxt.gameObject.SetActive(false);
+        scoreTxt.gameObject.SetActive(false);
+        finalTime = time;
+        score += (int)700000 / finalTime;
+
+    }
+    void winSequence()
+    {
+        winner.SetActive(true);
+        btns.SetActive(false);
+        timeTxt.gameObject.SetActive(false);
+        scoreTxt.gameObject.SetActive(false);
+        finalTime = time;
+        score += (int)700000 / finalTime;
+        winner.transform.GetChild(0).gameObject.GetComponent<Text>().text = "\t  You've won \nFinal Score: " + score.ToString() + "\n Final Time: " + finalTime.ToString();
     }
     public static Card getTempCard()
     {
